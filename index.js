2d2dcf3bafd11009544e0103395d5c03c4f35ca2
shadhirsha <75199052+shadhirsha@users.noreply.github.com>
2025-08-11 06:51:56 +0530

    shadhir v4 with pm2 start

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node shadhir v3!\n';
+  const msg = 'Hello Node shadhir v4 with pm2 start!\n';
   res.end(msg);
 });
 
