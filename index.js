7bb5bf644ad2bc5cfca36bae5a4bb04c77297f0a
shadhirsha <75199052+shadhirsha@users.noreply.github.com>
2025-08-12 15:33:31 +0530

    update SSL and deploy test

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node shadhir CI/CD working self hosted!\n';
+  const msg =
+    'Hello Node shadhir CI/CD working self hosted after update SSL with NGINX!\n';
   res.end(msg);
 });
 
