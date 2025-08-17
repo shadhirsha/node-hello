328886f4045906c6ba06606ef0e2494ef5d9bfbc
shadhirsha <75199052+shadhirsha@users.noreply.github.com>
2025-08-17 18:21:37 +0530

    check whether .env file working on docker

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+const stage = process.env.STAGE || 'development';
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg =
-    'Hello Node shadhir CI/CD working EC2-SSH Removed Action Runner\n';
+  const msg = `Hello Node CI/CD working EC2-SSH Removed Action Runner ${stage}\n`;
   res.end(msg);
 });
 
